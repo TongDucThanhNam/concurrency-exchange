3e040ac627ad02a176b36aa873dd7b83138d3ea8
tongducthanhnam <tongducthanhnam@gmail.com>
2024-11-09 14:11:42 +0700

    Finished

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -1,11 +1,63 @@
 'use client'
 
-import React, {useState} from 'react'
-import {Avatar, Button, Input, Select, SelectItem, Tooltip} from '@nextui-org/react'
-import {CheckIcon} from '@nextui-org/shared-icons'
+import React, { useState } from 'react'
+import {
+    Avatar,
+    Button,
+    Input,
+    Select,
+    type SelectedItems,
+    SelectItem,
+    type SharedSelection,
+    Spacer
+} from '@nextui-org/react'
+import { CheckIcon } from '@nextui-org/shared-icons'
+import {priceVietNamDongformetter} from "../lib/utils.tsx";
 
-const currencies = [
-    {label: 'USD', value: 'USD', country: 'United States', icon: "us", key: "USD"},
+type Currency = {
+    label: string
+    value: string
+    country: string
+    icon: string
+    key: string
+}
+
+const currencies: Currency[] = [
+    {
+        label: 'USD',
+        value: 'USD',
+        country: 'Đô la Mỹ',
+        icon: 'us',
+        key: 'USD',
+    },
+    {
+        label: 'EUR',
+        value: 'EUR',
+        country: 'EURO',
+        icon: 'eu',
+        key: 'EUR',
+    },
+    {
+        label: 'GBP',
+        value: 'GBP',
+        country: 'Bảng Anh',
+        icon: 'gb',
+        key: 'GBP',
+    },
+    {
+        label: 'JPY',
+        value: 'JPY',
+        country: 'Yên Nhật',
+        icon: 'jp',
+        key: 'JPY',
+    },
+    {
+        label: 'CNY',
+        value: 'CNY',
+        country: 'Nhân dân tệ',
+        icon: 'cn',
+        key: 'CNY',
+    },
 ]
 
 export default function HeroSection() {
@@ -14,12 +66,8 @@ export default function HeroSection() {
     const [convertedAmount, setConvertedAmount] = useState('')
 
     const handleExchange = () => {
-        // Here you would typically call an API to get the current exchange rate
-        // and calculate the converted amount. For now, we'll just log the input.
-        //https://api.currencylayer.com/convert?from=EUR&to=GBP&amount=100
         console.log(`Converting ${amount} ${currency} to VND`)
 
-        //Fetch API
         fetch(`https://api.currencylayer.com/convert?access_key=0dcda163fa387910b11a09eb6a8dbe32&from=${currency}&to=VND&amount=${amount}`)
             .then(response => response.json())
             .then(data => {
@@ -29,69 +77,78 @@ export default function HeroSection() {
     }
 
     return (
-        <section className="h-full w-full bg-gradient-to-r from-blue-500 to-purple-600 text-white py-20">
+        <section className="min-h-screen w-full flex items-center justify-center bg-gradient-to-r from-slate-500 to-slate-800">
             <div className="container mx-auto px-4">
-                <h1 className="text-4xl font-bold mb-6 text-center">
-                    Chuyển đổi tiền tệ nhanh chóng
-                </h1>
-                <p className="text-xl mb-8 text-center">
-                    Chuyển đổi các đơn vị tiền tệ phổ biến sang VND với tỷ giá hôm nay
-                </p>
-                <div
-                    className="max-w-3xl mx-auto bg-white/10 backdrop-blur-md rounded-lg p-6 shadow-lg justify-center items-center">
-                    <div className="flex flex-col md:flex-row gap-4 items-center">
-                        <Input
-                            type="number"
-                            width={10}
-                            label="Số tiền"
-                            placeholder="Nhập số tiền cần chuyển đổi"
-                            value={amount}
-                            onValueChange={setAmount}
-                            className="flex-grow w-32"
-                            classNames={{
-                                input: 'text-white',
-                                label: 'text-white',
-                            }}
-                        />
-                        <Select
-                            label="Đơn vị tiền tệ"
-                            placeholder="Chọn đơn vị tiền tệ"
-                            selectedKeys={[currency]}
-                            selectionMode={"single"}
-                            onSelectionChange={(keys) => setCurrency(Array.from(keys)[0] as string)}
-
-                            className="md:w-1/3 w-48"
-                        >
-                            {currencies.map((cur) => (
-                                // <SelectItem key={cur.value} value={cur.value}>
-                                //     {cur.label}
-                                // </SelectItem>
-                                <SelectItem
-                                    key={cur.key}
-                                    value={cur.label}
-                                    startContent={<Avatar alt="Argentina" className="w-6 h-6"
-                                                          src={`https://flagcdn.com/${cur.icon}.svg`}/>}
-                                >
-                                    [{cur.label}]
-                                </SelectItem>
-
-                            ))}
-                        </Select>
-                        <div className={"h-full w-32"}>
-                            <Tooltip content="Chuyển đổi" placement="top">
-                                <Input
-                                    value={convertedAmount}
-                                    label="VND"
-                                />
-                            </Tooltip>
+                <div className="flex flex-col items-center justify-center">
+                    <h1 className="text-4xl font-bold mb-6 text-center text-white">
+                        Chuyển đổi tiền tệ nhanh chóng
+                    </h1>
+                    <p className="text-xl mb-8 text-center text-white">
+                        Chuyển đổi các đơn vị tiền tệ phổ biến sang VND với tỷ giá hôm nay
+                    </p>
+                    <div className="w-full max-w-3xl mx-auto bg-white/10 backdrop-blur-md rounded-lg p-6 shadow-lg">
+                        <div className="flex flex-col md:flex-row gap-4 items-center">
+                            <Input
+                                type="number"
+                                label="Số tiền"
+                                placeholder="Nhập số tiền cần chuyển đổi"
+                                value={amount}
+                                onValueChange={setAmount}
+                                className="flex-grow"
+                                classNames={{
+                                    input: 'text-white',
+                                    label: 'text-white',
+                                }}
+                            />
+                            <Select
+                                items={currencies}
+                                label="Loại tiền tệ"
+                                placeholder="Chọn loại tiền"
+                                className="w-full md:w-full"
+                                classNames={{
+                                    trigger: "h-12",
+                                }}
+                                onSelectionChange={(items: SharedSelection) => {
+                                    // console.log(items.currentKey);
+                                    // @ts-ignore
+                                    setCurrency(items.currentKey);
+                                }}
+                                renderValue={(items: SelectedItems<Currency>) => {
+                                    return items.map((item) => (
+                                        <div key={item.key} className="flex flex-row">
+                                            <Avatar alt={item.data?.country} className="w-6 h-6" src={`https://flagcdn.com/${item.data?.icon}.svg`} />
+                                            <Spacer x={2}/>
+                                            {item.data?.value}
+                                        </div>
+                                    ));
+                                }}
+                            >
+                                {(currency) => (
+                                    <SelectItem
+                                        key={currency.key}
+                                        value={currency.label}
+                                        onSelect={() => setCurrency(currency.value)}
+                                        startContent={<Avatar alt={currency.country} className="w-6 h-6" src={`https://flagcdn.com/${currency.icon}.svg`} />}
+                                    >
+                                        {currency.country} [{currency.label}]
+                                    </SelectItem>
+                                )}
+                            </Select>
+                            <Input
+                                value={priceVietNamDongformetter(convertedAmount)}
+                                label="VND"
+                                className="w-full md:w-full"
+                            />
+                            <Button
+                                className="h-14 w-full md:w-auto"
+                                color="primary"
+                                variant="shadow"
+                                endContent={<CheckIcon />}
+                                onClick={handleExchange}
+                            >
+                                Chuyển đổi
+                            </Button>
                         </div>
-
-                        <Button
-                            endContent={<CheckIcon/>}
-                            onClick={handleExchange}
-                        >
-                            Chuyển đổi
-                        </Button>
                     </div>
                 </div>
             </div>
